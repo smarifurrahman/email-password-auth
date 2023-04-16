560b5d85074a5011858df6f50b76dd27788a6862
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-04-16 17:01:46 +0600

    event handler added in registerRBS

--- src/components/RegisterRBS/RegisterRBS.jsx
+++ src/components/RegisterRBS/RegisterRBS.jsx
@@ -3,23 +3,29 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const RegisterRBS = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const check = e.target.check.value;
+        console.log(email, password, check)
+    }
+
     return (
         <div>
-            <Form className='px-5 py-3 mx-5 my-3'>
+            <h3 className='text-center text-primary'>Please Register</h3>
+            <Form onSubmit={handleSubmit} className='px-5 py-3 mx-5 my-3'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+                    <Form.Control type="email" name="email" placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" name="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
+                    <Form.Check type="checkbox" name="check" label="Accept Terms and Conditions" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
