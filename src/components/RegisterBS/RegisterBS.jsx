2f08e0f263633b7a38908b95ef80889de5a32f58
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-04-16 20:06:25 +0600

    fixed

--- src/components/RegisterBS/RegisterBS.jsx
+++ src/components/RegisterBS/RegisterBS.jsx
@@ -12,18 +12,20 @@ const RegisterBS = () => {
 
     return (
         <div>
+            <h3 className='text-center text-primary'>Please Register</h3>
+
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="inputEmail1" className="form-label">Email address</label>
-                    <input type="email" name="email" className="form-control" id="inputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" name="email" className="form-control" id="inputEmail1" placeholder='abc@xyz.com' aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="inputPassword1" className="form-label">Password</label>
-                    <input type="password" name="password" className="form-control" id="inputPassword1" />
+                    <input type="password" placeholder='********' name="password" className="form-control" id="inputPassword1" />
                 </div>
                 <div className="mb-3 form-check">
                     <input type="checkbox" name="check" className="form-check-input" id="check1" />
-                    <label className="form-check-label" htmlFor="check1">Check me out</label>
+                    <label className="form-check-label" htmlFor="check1">Accept Terms and Conditions</label>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
--- src/components/Resigter/Register.jsx
+++ src/components/Resigter/Register.jsx
@@ -24,13 +24,13 @@ const Register = () => {
 
     return (
         <div>
-            <h4>Please Register</h4>
+            <h4 className='text-primary'>Please Register</h4>
             <form onSubmit={handleSubmit}>
-                <input onChange={handleChange} type="email" name="email" id="email" placeholder="Your Email" />
+                <input className="px-4 form-control" onChange={handleChange} type="email" name="email" id="email" placeholder="Your Email" />
                 <br />
-                <input onBlur={handleBlur} type="password" name="password" id="password" placeholder="Your Password" />
+                <input className="px-4 form-control" onBlur={handleBlur} type="password" name="password" id="password" placeholder="Your Password" />
                 <br />
-                <input type="submit" value="Register" />
+                <input className='btn btn-primary' type="submit" value="Register" />
             </form>
         </div>
     );
